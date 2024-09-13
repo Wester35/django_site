760296146d98378f5c9b9f409d100ecc94ee4068
Wester35 <shum0302@gmail.com>
2024-09-13 22:29:26 +0300

    tst

--- server/main/urls.py
+++ server/main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import HomePageView
+from django.conf.urls.static import static
+from server import settings
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/main/views.py
+++ server/main/views.py
@@ -1,5 +1,5 @@
+from django.conf import settings
 from django.views.generic import TemplateView
-
 
 class HomePageView(TemplateView):
     template_name = 'main/index.html'
@@ -7,4 +7,5 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['name_button'] = 'Дарова'
+        context['MEDIA_URL'] = settings.MEDIA_URL  # Добавляем MEDIA_URL в контекст
         return context
